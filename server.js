77d22dd7fddf9b1c26ec2d36db7c424f6e05bc61
Khemraj Thapa <khyamay@gmail.com>
2014-06-11 16:26:52 +0100

    added delete, post, put and get route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 var Bear = require('./app/models/bear');
 
 mongoose.connect('localhost');
+
+
 app.use(bodyParser());
 
 
@@ -25,10 +27,59 @@ router.get('/', function(req, res){
 	
 });
 
+router.route('/bears')
+	.post(function(req, res){
+		var bear = new Bear();
+		bear.name = req.body.name;
+
+		bear.save(function(err){
+			if (err)
+				res.send(err);
+
+			res.json({message: 'Bear created!'});
+		});
+	})
+	.get(function(req, res){
+		Bear.find(function(err, bears){
+			if (err) 
+				res.send(err);
+			res.json(bears);
+		});
+	});
+
+router.route('/bears/:bear_id')
+	.get(function(req, res){
+		Bear.findById(req.params.bear_id, function(err, bear){
+			if (err)
+				res.send(err);
+			res.json(bear);
+		});
+	})
+	.put(function(req, res){
+		Bear.findById(req.params.bear_id, function(err, bear){
+			if(err)
+				res.send(err);
+			bear.name = req.body.name;
+
+			bear.save(function(err){
+				if(err)
+					res.send(err);
+				res.json({message: 'Bear updated!'});
+			});
+
+		});
+	})
+	.delete(function(req, res){
+		Bear.remove({
+			_id : req.params.bear_id
+		}, function(err, bear){
+			if(err)
+				res.send(err);
+			res.json({message: 'Successfully deleted!'});
+		});
+	});
+
 app.use('/api', router);
-
-
-
 
 app.listen(port);
 console.log('Magic happens on port' + port);
