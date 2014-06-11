f3fc693e7d97997d4649f9b0f6811253932b564d
Khemraj Thapa <khyamay@gmail.com>
2014-06-11 16:02:23 +0100

    added model

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Bear = require('./app/models/bear');
 
+mongoose.connect('localhost');
 app.use(bodyParser());
+
+
 
 var port = process.env.PORT || 3000;
 
 
 var router = express.Router();
+
+router.use(function(req, res, next){
+	console.log('Something is happening');
+	next();
+});
 
 router.get('/', function(req, res){
 
@@ -16,6 +26,9 @@ router.get('/', function(req, res){
 });
 
 app.use('/api', router);
+
+
+
 
 app.listen(port);
 console.log('Magic happens on port' + port);
